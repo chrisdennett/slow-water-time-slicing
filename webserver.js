85f2e7d5f625462f9f4655dab2be050a67315afb
chrisdennett <chrisdennett@gmail.com>
2022-02-19 13:06:34 +0000

    Removed unused socket.io reference

--- webserver.js
+++ webserver.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
 
 app.use(express.static(__dirname + "/app/"));
 
